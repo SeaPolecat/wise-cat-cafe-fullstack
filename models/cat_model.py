b0899dde657bf9f9f19fa617296014ef99d1c211
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-05-10 00:17:14 -0400

    added columns and __repr__

---
+++ models/cat_model.py
@@ -0,0 +1,14 @@
+from app import db
+
+from datetime import datetime, timezone
+
+class Cat(db.Model):
+
+    id = db.Column(db.Integer, primay_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    image = db.Column(db.String, nullable=False)
+    wisdom = db.Column(db.String, nullable=False)
+    date_summoned = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return f'{self.id}) {self.name}. Summoned on {self.date_summoned}.'
