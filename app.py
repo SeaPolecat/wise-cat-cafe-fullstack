2ef53bae1b5db7ba211112aea92cdb1e2847f450
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-05-09 23:50:09 -0400

    wrapped everything in a create_app function and made the db

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+from config import ProductionConfig
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def create_app(config_class=ProductionConfig):
+
+    app = Flask(__name__)
+
+    # configure database
+    app.config.from_object(config_class)
+
+    db = SQLAlchemy(app)
+
+    with app.app_context():
+        db.create_all()
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
+
+    app.logger.info("Starting Flask app...")
+
     app.run(debug=True, host='0.0.0.0', port=5002)
