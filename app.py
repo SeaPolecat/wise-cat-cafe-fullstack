ab53119f583834bc46e1f81a1bb05dd4af65081f
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-05-10 00:18:51 -0400

    made db a global object so other classes can use it too

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import ProductionConfig
 
+# uninitialized instance of db
+db = SQLAlchemy()
+
 def create_app(config_class=ProductionConfig):
 
     app = Flask(__name__)
@@ -10,8 +13,10 @@ def create_app(config_class=ProductionConfig):
     # configure database
     app.config.from_object(config_class)
 
-    db = SQLAlchemy(app)
+    # initialize database
+    db.init_app(app)
 
+    # recreate all tables
     with app.app_context():
         db.create_all()
 
