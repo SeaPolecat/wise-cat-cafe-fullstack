71133c0cc3b420b971ba361aeee08ef301a139a7
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-05-19 22:05:57 -0400

    made constructor for Cat object

--- models/cat_model.py
+++ models/cat_model.py
@@ -8,10 +8,13 @@ class Cat(db.Model):
     name = db.Column(db.String(50), nullable=False)
     image = db.Column(db.String, nullable=False)
     wisdom = db.Column(db.String, nullable=False)
-    date_summoned = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    date_summoned = db.Column(db.DateTime)
 
     def __repr__(self):
         return f'{self.id}) {self.name}. Summoned on {self.date_summoned}.'
     
     def __init__(self):
-        pass
+        self.name = ''
+        # self.image = api call
+        # self.wisdom = api call
+        self.date_summoned = datetime.now(timezone.utc)
