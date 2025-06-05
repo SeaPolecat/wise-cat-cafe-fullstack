18c4b8228d92e2fb14875692fb82b3213edf090d
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-06-05 17:32:31 +0800

    created get_cat_image that gets images from cat api

--- utils/api_utils.py
+++ utils/api_utils.py
@@ -1,4 +1,15 @@
+import os
+import requests
+from dotenv import load_dotenv
 
+load_dotenv()
 
-#def get_cat_image() -> str:
-    
+CAT_API_URL=os.getenv('CAT_API_URL')
+
+def get_cat_image() -> str:
+    response = requests.get(CAT_API_URL)
+    data = response.json()
+
+    cat_image = data[0].get('url')
+
+    return cat_image
