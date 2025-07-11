94813fc4e89431100aed8a017933d5fb008da9c5
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-07-11 13:55:05 +0800

    fixed typos, added summon_cat method, along with to_dict and to_cat to turn a Cat object to and from a dict

--- models/cat_model.py
+++ models/cat_model.py
@@ -1,20 +1,60 @@
-from app import db
+from flask import session
+
+from db import db
 
 from datetime import datetime, timezone
 
-class Cat(db.Model):
+class Cats(db.Model):
 
-    id = db.Column(db.Integer, primay_key=True)
+    __tablename__ = 'cats'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
     image = db.Column(db.String, nullable=False)
     wisdom = db.Column(db.String, nullable=False)
     date_summoned = db.Column(db.DateTime)
 
     def __repr__(self):
-        return f'{self.id}) {self.name}. Summoned on {self.date_summoned}.'
+        return f'{self.id}) {self.name}. Summoned on {self.date_summoned.date()}.'
     
-    def __init__(self):
+    def __init__(self, image: str, wisdom: str):
         self.name = ''
-        # self.image = api call
-        # self.wisdom = api call
+        self.image = image
+        self.wisdom = wisdom
         self.date_summoned = datetime.now(timezone.utc)
+
+    def to_dict(self) -> dict[str, any]:
+        return {
+            'name': self.name,
+            'image': self.image,
+            'wisdom': self.wisdom,
+            'date_summoned': self.date_summoned
+        }
+    
+    @classmethod
+    def to_cat(cls, data: dict[str, any]) -> 'Cats':
+        return cls(**data)
+
+
+    @classmethod
+    def summon_cat(cls, img: str, wisdom: str) -> None:
+        new_cat = Cats(img, wisdom)
+
+        # store a cache of cats in the flask session, 
+        # a dict that's unique to each user
+
+        if not session.get('cache'):
+            session['cache'] = [new_cat.to_dict()] # turn object to dict, because session can only store JSON values
+
+        else:
+            # session vars are immutable, so we must 
+            # make a new cache every time
+            cache = session['cache']
+
+            cache.insert(0, new_cat.to_dict())
+
+            # limit cache size to 2
+            if len(cache) > 2:
+                cache.pop(-1)
+
+            session['cache'] = cache
