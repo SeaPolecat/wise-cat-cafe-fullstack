177cb19decc294783d548ae8972d772c011f590d
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-07-25 08:55:48 +0800

    added constructor that uses werkzeug security to generate a hashed password, added create_user method

--- models/user_model.py
+++ models/user_model.py
@@ -1,4 +1,5 @@
 from db import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
 
@@ -6,8 +7,11 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    salt = db.Column(db.String(32), nullable=False)  # 16-byte salt in hex
-    password = db.Column(db.String(64), nullable=False)  # SHA-256 hash in hex
+    hashed_password = db.Column(db.String(64), nullable=False)
+
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.hashed_password = generate_password_hash(password)
 
 
     @staticmethod
@@ -36,3 +40,13 @@ class User(db.Model):
             errors['confirmed_pass_error'] = "This did not match the password above"
 
         return errors
+    
+
+    @classmethod
+    def create_user(cls, username: str, password: str) -> None:
+        """Creates a new user to add to the database.
+        """
+        new_user = User(username=username, password=password)
+
+        db.session.add(new_user)
+        db.session.commit()
